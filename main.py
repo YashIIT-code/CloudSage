0084298359d700befdb490d0f8603ecdbe24bebb
Yash Srivastava <proyash1611@gmail.com>
2026-03-11 15:15:48 +0530

    fix uvicorn path

--- main.py
+++ main.py
@@ -47,4 +47,4 @@ def read_root():
 # Enable local testing and fast feedback loop (python -m backend.main)
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run("backend.main:app", host="0.0.0.0", port=8000, reload=True)
+    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
